5c02e4ee6ab7b5e3162fef1aac3614ab04a78789
‪sufian nasser‬‏ <166199800+sufyanaser@users.noreply.github.com>
2026-06-30 13:16:11 +0300

    Route v2 experience from app entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import V2 from './V2'
 import './styles.css'
+import './v2.css'
+
+const Root = window.location.pathname.startsWith('/v2') ? V2 : App
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
