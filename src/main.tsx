1db10119ae5b21e73c1a05caf81b4d2f24139f22
‪sufian nasser‬‏ <166199800+sufyanaser@users.noreply.github.com>
2026-06-30 13:25:49 +0300

    Remove v2 route and restore official site entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import V2 from './V2'
 import './styles.css'
-import './v2.css'
-
-const Root = window.location.pathname.startsWith('/v2') ? V2 : App
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Root />
+    <App />
   </StrictMode>,
 )
